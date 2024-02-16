c752ee1cf0529a508a69f43b38b607a1ccb0ad9a
catttjyl <youlunj2@illinois.ed>
2024-02-15 21:57:17 -0600

    Reset up on heroku and netlify

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://chat-app-local-50b3342ce0bd.herokuapp.com";
-export const host = "http://localhost:3001";
+export const host = "https://chat-app-local-50b3342ce0bd.herokuapp.com";
+// export const host = "http://localhost:3001";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
